065ab1bf9cfaf5b4b7a39ea8366792fb1599b89e
Errodion <kuzminrd44@gmail.com>
2023-09-10 13:29:11 +0300

    Lab_2_1

---
+++ lab_2/counter.py
@@ -0,0 +1,103 @@
+import os
+from collections import Counter
+import math
+import re
+
+pattern = re.compile(r'[а-яА-ЯёЁ]')
+
+def read_content(path, mode='all'):
+    with open(path, 'rb') as file:
+        content = file.read()
+
+    if mode == 'all':
+        return content
+    elif mode == 'utf-8':
+        return content.decode('utf-8')
+
+
+def calculate_all(path):
+    content = read_content(path)
+    calculate_file_statistics(content)
+
+
+def calculate_utf_8(path):
+    content = read_content(path, mode='utf-8')
+    calculate_file_statistics(content)
+
+
+def calculate_file_statistics(content):
+    file_length = len(content)
+
+    symbol_frequencies = Counter(content)
+
+    probabilities = {symbol: frequency / file_length for symbol, frequency in symbol_frequencies.items()}
+
+    information_content = {symbol: -1 * (symbol_frequencies[symbol] * prob * math.log2(prob)) for symbol, prob in
+                           probabilities.items()}
+
+    total_info_bits = sum(information_content.values())
+    total_info_bytes = total_info_bits / 8
+
+    sorted_alphabet = sorted(probabilities.items(), key=lambda x: x[0])
+
+    sorted_frequencies = sorted(probabilities.items(), key=lambda x: x[1], reverse=True)
+
+    print(f"File content length: {file_length}")
+    print("Table (by letters):")
+    print("Symbol\tFrequency\tProbabilities\tInformation")
+    print_table(information_content, probabilities, sorted_alphabet)
+    print("\nTable (by frequency):")
+    print("Symbol\tFrequency\tProbabilities\tInformation")
+    print_table(information_content, probabilities, sorted_frequencies)
+    print("\nAmount of information:")
+    print(f"bits: {total_info_bits}")
+    print(f"bytes: {total_info_bytes}")
+
+
+def analyze_file(path):
+    octet_frequencies = Counter(read_content(path))
+
+    # Наиболее частые октеты
+    most_common_octets = octet_frequencies.most_common(4)
+
+    # Наиболее частые октеты, не являющиеся кодами печатных символов ASCII
+    non_printable_ascii_octets = [octet for octet, _ in octet_frequencies.most_common() if octet < 32 or octet > 126][
+                                 :4]
+
+    # Вывод результатов
+    print(f"Octets: {most_common_octets}")
+    print(f"Octets (not ASCII): {non_printable_ascii_octets}")
+
+    is_russian(octet_frequencies)
+
+
+def print_table(information_content, probabilities, dictionary):
+    for symbol, frequency in dictionary:
+        prob = round(probabilities[symbol], 10)
+        info = round(information_content[symbol], 10)
+        frequency = round(frequency, 10)
+        print(f"{symbol}\t{frequency}\t{prob}\t{info}")
+
+
+def is_russian(octet_frequencies):
+    for encoding_name in ['utf-8', 'cp1251', 'koi8-r']:
+        try:
+            text = bytes(octet_frequencies.keys()).decode(encoding_name)
+            if pattern.search(text):
+                print(f"Russian text (encoding - {encoding_name})")
+        except UnicodeDecodeError:
+            continue
+
+
+# file_path = '/home/errodion/Projects/Encoding/otik/labs-files/Файлы в разных форматах/beep.ogg'
+# file_path = '/home/errodion/Projects/Encoding/otik/labs-files/Файлы в формате простого текста — utf8/Rewards and Fairies, by Rudyard Kipling.txt'
+file_path = '/home/errodion/Projects/Encoding/otik/labs-files/Файлы в формате простого текста — кодировки разные/Алфавит — koi8r.txt'
+
+if os.path.exists(file_path):
+    # print('---|ALL|---')
+    # calculate_all(file_path)
+    # print('---|UTF-8|---')
+    # calculate_utf_8(file_path)
+    analyze_file(file_path)
+else:
+    print("File not found")
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from lab_1.encoder import Encoder
-from lab_1.decoder import Decoder
-
-encoder = Encoder()
-decoder = Decoder()
-encoder.create_archive()
-decoder.read_archive()
+# from lab_1.encoder import Encoder
+# from lab_1.decoder import Decoder
+from lab_2 import counter
+# encoder = Encoder()
+# decoder = Decoder()
+# encoder.create_archive()
+# decoder.read_archive()
